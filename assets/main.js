2b7995f87050247b62e583e7b8b2cb22340a61a4
mdtahidulislam <tahidul.developer@gmail.com>
2021-12-31 10:32:20 +0600

    added fontawesome icon,header section and style.css

--- assets/main.js
+++ assets/main.js
@@ -4,22 +4,22 @@
         soutcebook tab carousel
     =================================*/
     $(document).ready(function(){
-        $(".sourcebook-active").owlCarousel({
-            margin: 15,
-            dots: false,
-            loop: true,
-            nav: true,
-            responsive: {
-                0: {
-                    items: 1,
-                },
-                600: {
-                    items: 2,
-                },
-                1000: {
-                    items: 5,
-                }
-            }
-        });
+        // $(".sourcebook-active").owlCarousel({
+        //     margin: 15,
+        //     dots: false,
+        //     loop: true,
+        //     nav: true,
+        //     responsive: {
+        //         0: {
+        //             items: 1,
+        //         },
+        //         600: {
+        //             items: 2,
+        //         },
+        //         1000: {
+        //             items: 5,
+        //         }
+        //     }
+        // });
       });
 })(jQuery);
